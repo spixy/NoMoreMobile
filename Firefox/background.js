42af79668804dc50406d84b44df46b60a52caa1e
Henrich Horváth <dev@henrich-horvath.eu>
2018-03-16 00:14:50 +0100

    Firefox version

---
+++ Firefox/background.js
@@ -0,0 +1,65 @@
+function fastIndexOf(array, item)
+{
+  var i = array.length;
+  while (i--)
+  {
+     if (array[i] === item)
+         return i;
+  }
+  return -1;
+}
+
+function tryGetNewUrl(url)
+{
+  var parts = url.split("//");
+
+  if (parts.length !== 2)
+  {
+    return null;
+  }
+
+  var newUrl = parts[0];
+  parts = parts[1].split(".");
+  var m_index = fastIndexOf(parts, "m");
+
+  // ".m." cannot be in 1st and 2nd level domain
+  if (m_index === -1 || m_index > parts.length - 3)
+  {
+    return null;
+  }
+
+  return createNewUrl(parts, m_index, newUrl);
+}
+
+function createNewUrl(parts, m_index, newUrl)
+{
+  var last_index = parts.length - 1;
+
+  newUrl += "//";
+  for (var i = 0; i < m_index; ++i)
+  {
+    newUrl += parts[i] + ".";
+  }
+  for (var i = m_index + 1; i < last_index; ++i)
+  {
+    newUrl += parts[i] + ".";
+  }
+
+  return newUrl + parts[last_index];
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+    function(details)
+    {
+        var host = tryGetNewUrl(details.url);
+        if (host !== null)
+        {
+          return { redirectUrl: host };
+        }
+    },
+    {
+        urls: ["<all_urls>"],
+        types: ["main_frame"]
+    },
+    ["blocking"]
+);
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # No More Mobile
 Redirects mobile pages to classic (desktop) pages
 
-**Download:** [v0.1 (zip)](https://github.com/spixy/NoMoreMobile/releases/download/0.1/NoMoreMobile.zip)
+### Download
+[Google Chrome](https://github.com/spixy/NoMoreMobile/releases/download/0.1/Chrome.zip)
+
+[Mozilla Firefox](https://addons.mozilla.org/firefox/addon/no-more-mobile/)
 
 Icon made by [Freepik](https://www.flaticon.com/) is licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/)
