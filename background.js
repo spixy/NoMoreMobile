c9bfd8b7c504cd9fad0f083aca7573aa249fd309
Henrich Horváth <dev@henrich-horvath.eu>
2018-03-15 21:14:23 +0100

    update

--- background.js
+++ background.js
@@ -1,16 +1,23 @@
-function redirect(tab)
+function getNewUrl(url)
 {
-  var tabUrl = encodeURIComponent(tab.url);
-  var parts = tabUrl.split(".");
+  var parts = url.split("//");
 
-  if (parts.length < 2)
+  if (parts.length != 2)
   {
     console.error("Invalid URL");
-    return;
+    return null;
+  }
+
+  var newUrl = parts[0] + "//";
+  var parts = parts[1].split(".");
+
+  if (parts.length == 0)
+  {
+    console.error("Invalid URL");
+    return null;
   }
 
   var lastIndex = parts.length - 1;
-  var newUrl = "";
   var found = false;
 
   for (var i = 0; i < lastIndex; i++)
@@ -26,14 +33,22 @@ function redirect(tab)
       found = true;
     }
   }
-  
+
   newUrl += parts[lastIndex];
-
-  chrome.tabs.update(tab.id, {url: newUrl});
+  return newUrl;
 }
 
-chrome.tabs.onUpdated.addListener(onTabChanged);
-
-function onTabChanged() {
-  chrome.tabs.getCurrent(redirect);
-}
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details)
+    {
+        var host = getNewUrl(details.url);
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+          "*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
