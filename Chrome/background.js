3e744edaa121ff21968d4b214629bf7ae5aa96c4
Henrich Horváth <dev@henrich-horvath.eu>
2018-05-06 21:52:48 +0200

    Chrome - added locales and blacklist feature

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,3 +1,7 @@
+var blackList = [];
+var blackListSize = 0;
+var useBlackList = false;
+
 function fastIndexOf(array, item)
 {
   var i = array.length;
@@ -12,7 +16,6 @@ function fastIndexOf(array, item)
 function tryGetNewUrl(url)
 {
   var parts = url.split("//");
-
   if (parts.length !== 2)
   {
     return null;
@@ -28,30 +31,50 @@ function tryGetNewUrl(url)
     return null;
   }
 
+  // Check in blacklist
+  if (useBlackList) {
+    for (let i = 0; i < blackListSize; ++i)
+    {
+      if (url.includes(blackList[i]))
+      {
+        return null;
+      }
+    }
+  }
+
+  // create non-mobile URL
   return createNewUrl(parts, m_index, newUrl);
 }
 
 function createNewUrl(parts, m_index, newUrl)
 {
   var last_index = parts.length - 1;
-
   newUrl += "//";
-  for (var i = 0; i < m_index; ++i)
+  for (let i = 0; i < m_index; ++i)
   {
     newUrl += parts[i] + ".";
   }
-  for (var i = m_index + 1; i < last_index; ++i)
+  for (let i = m_index + 1; i < last_index; ++i)
   {
     newUrl += parts[i] + ".";
   }
-
   return newUrl + parts[last_index];
 }
+
+// laod options
+chrome.storage.sync.get({
+  urls: [],
+  enabled: false
+  }, function(items) {
+    blackList = items.urls;
+    blackListSize = items.urls.length;
+    useBlackList = items.enabled;
+});
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details)
     {
-        var host = tryGetNewUrl(details.url);
+        const host = tryGetNewUrl(details.url);
         if (host !== null)
         {
           return { redirectUrl: host };
@@ -63,3 +86,16 @@ chrome.webRequest.onBeforeRequest.addListener(
     },
     ["blocking"]
 );
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (key in changes) {
+    var storageChange = changes[key];
+
+    if (key === "urls") {
+      blackList = storageChange.newValue;
+      blackListSize = blackList.length;
+    } else {
+      useBlackList = storageChange.newValue;
+    }
+  }
+});
